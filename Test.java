2e063c41181b61f38f4990869b433fc638778776
SantuHB <santhosh.proxy@gmail.com>
2020-04-14 23:06:13 +0530

    Test Class Committed

--- Test.java
+++ Test.java
@@ -4,12 +4,9 @@ public class Test {
 	public static void main(String[] args)
 	{
 	System.out.println("WELCOME");
-	System.out.println(System.getProperty("java.version"));
-    System.out.println(System.getProperty("java.runtime.version"));
-    System.out.println(System.getProperty("java.home"));
-    System.out.println(System.getProperty("java.vendor"));
-    System.out.println(System.getProperty("java.vendor.url"));
-    System.out.println(System.getProperty("java.class.path"));
+	System.out.println("JDK Version : " +System.getProperty("java.version"));
+    	System.out.println("JRE Version : " +System.getProperty("java.runtime.version"));
+    
 
 
 	}
